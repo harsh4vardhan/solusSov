75c2f8fbe63508e47de0d2bfe6b44a55d6845171
harsh4vardhan <hvardhan609@gmail.com>
2023-05-29 06:59:38 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,120 @@
+function about() {
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.display = 'none';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.display = 'block';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.transition = 'opacity 2s linear';
+        elmsArchive[i].style.display = 'none';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsEvents[i].style.transition = 'opacity 2s linear';
+        elmsEvents[i].style.display = 'none';
+    }
+}
+
+function home() {
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.transition = 'opacity 2s linear';
+        elmsHome[i].style.display = 'block';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.transition = 'opacity 2s linear';
+        elmsAbout[i].style.display = 'none';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.transition = 'opacity 2s linear';
+        elmsArchive[i].style.display = 'none';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsEvents[i].style.transition = 'opacity 2s linear';
+        elmsEvents[i].style.display = 'none';
+    }
+    var elmsTickets = document.querySelectorAll("[id='buyTickets']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsTickets[i].style.display = 'none';
+    }
+}
+
+function archive() {
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.display = 'none';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.display = 'none';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.transition = 'opacity 2s linear';
+        elmsArchive[i].style.display = 'block';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsEvents[i].style.transition = 'opacity 2s linear';
+        elmsEvents[i].style.display = 'none';
+    }
+    var elmsTickets = document.querySelectorAll("[id='buyTickets']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsTickets[i].style.display = 'none';
+    }
+}
+
+function events() {
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.display = 'none';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.display = 'none';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.transition = 'opacity 2s linear';
+        elmsArchive[i].style.display = 'none';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsEvents[i].style.transition = 'opacity 2s linear';
+        elmsEvents[i].style.display = 'block';
+    }
+    var elmsTickets = document.querySelectorAll("[id='buyTickets']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsTickets[i].style.display = 'none';
+    }
+}
+
+function buyTickets() {
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.display = 'none';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.display = 'none';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.display = 'none';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsEvents[i].style.display = 'none';
+    }
+    var elmsTickets = document.querySelectorAll("[id='buyTickets']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsTickets[i].style.display = 'Block';
+    }
+}
