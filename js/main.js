3a3919584edf1ddb929ebbe9ff1e8fb3299e6322
harsh4vardhan <hvardhan609@gmail.com>
2023-06-06 05:32:07 +0530

    beta build

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function about() {
+    window.scrollTo(0, 0);
     var elmsHome = document.querySelectorAll("[id='home']");
     for (var i = 0; i < elmsHome.length; i++) {
         elmsHome[i].style.display = 'none';
@@ -8,45 +9,49 @@ function about() {
         elmsAbout[i].style.display = 'block';
     }
     var elmsArchive = document.querySelectorAll("[id='archive']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsArchive[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsArchive.length; i++) {
         elmsArchive[i].style.display = 'none';
     }
     var elmsEvents = document.querySelectorAll("[id='events']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsEvents[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsEvents.length; i++) {
         elmsEvents[i].style.display = 'none';
+    }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'none';
     }
 }
 
 function home() {
+    window.scrollTo(0, 0);
     var elmsHome = document.querySelectorAll("[id='home']");
     for (var i = 0; i < elmsHome.length; i++) {
-        elmsHome[i].style.transition = 'opacity 2s linear';
         elmsHome[i].style.display = 'block';
     }
     var elmsAbout = document.querySelectorAll("[id='about']");
     for (var i = 0; i < elmsAbout.length; i++) {
-        elmsAbout[i].style.transition = 'opacity 2s linear';
         elmsAbout[i].style.display = 'none';
     }
     var elmsArchive = document.querySelectorAll("[id='archive']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsArchive[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsArchive.length; i++) {
         elmsArchive[i].style.display = 'none';
     }
     var elmsEvents = document.querySelectorAll("[id='events']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsEvents[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsEvents.length; i++) {
         elmsEvents[i].style.display = 'none';
     }
     var elmsTickets = document.querySelectorAll("[id='buyTickets']");
-    for (var i = 0; i < elmsAbout.length; i++) {
+    for (var i = 0; i < elmsTickets.length; i++) {
         elmsTickets[i].style.display = 'none';
+    }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'none';
     }
 }
 
 function archive() {
+    window.scrollTo(0, 0);
     var elmsHome = document.querySelectorAll("[id='home']");
     for (var i = 0; i < elmsHome.length; i++) {
         elmsHome[i].style.display = 'none';
@@ -56,22 +61,25 @@ function archive() {
         elmsAbout[i].style.display = 'none';
     }
     var elmsArchive = document.querySelectorAll("[id='archive']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsArchive[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsArchive.length; i++) {
         elmsArchive[i].style.display = 'block';
     }
     var elmsEvents = document.querySelectorAll("[id='events']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsEvents[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsEvents.length; i++) {
         elmsEvents[i].style.display = 'none';
     }
     var elmsTickets = document.querySelectorAll("[id='buyTickets']");
-    for (var i = 0; i < elmsAbout.length; i++) {
+    for (var i = 0; i < elmsTickets.length; i++) {
         elmsTickets[i].style.display = 'none';
+    }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'none';
     }
 }
 
 function events() {
+    window.scrollTo(0, 0);
     var elmsHome = document.querySelectorAll("[id='home']");
     for (var i = 0; i < elmsHome.length; i++) {
         elmsHome[i].style.display = 'none';
@@ -82,17 +90,47 @@ function events() {
     }
     var elmsArchive = document.querySelectorAll("[id='archive']");
     for (var i = 0; i < elmsAbout.length; i++) {
-        elmsArchive[i].style.transition = 'opacity 2s linear';
         elmsArchive[i].style.display = 'none';
     }
     var elmsEvents = document.querySelectorAll("[id='events']");
-    for (var i = 0; i < elmsAbout.length; i++) {
-        elmsEvents[i].style.transition = 'opacity 2s linear';
+    for (var i = 0; i < elmsEvents.length; i++) {
         elmsEvents[i].style.display = 'block';
     }
     var elmsTickets = document.querySelectorAll("[id='buyTickets']");
-    for (var i = 0; i < elmsAbout.length; i++) {
+    for (var i = 0; i < elmsTickets.length; i++) {
         elmsTickets[i].style.display = 'none';
+    }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'none';
+    }
+}
+
+function merch() {
+    window.scrollTo(0, 0);
+    var elmsHome = document.querySelectorAll("[id='home']");
+    for (var i = 0; i < elmsHome.length; i++) {
+        elmsHome[i].style.display = 'none';
+    }
+    var elmsAbout = document.querySelectorAll("[id='about']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsAbout[i].style.display = 'none';
+    }
+    var elmsArchive = document.querySelectorAll("[id='archive']");
+    for (var i = 0; i < elmsAbout.length; i++) {
+        elmsArchive[i].style.display = 'none';
+    }
+    var elmsEvents = document.querySelectorAll("[id='events']");
+    for (var i = 0; i < elmsEvents.length; i++) {
+        elmsEvents[i].style.display = 'none';
+    }
+    var elmsTickets = document.querySelectorAll("[id='buyTickets']");
+    for (var i = 0; i < elmsTickets.length; i++) {
+        elmsTickets[i].style.display = 'none';
+    }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'block';
     }
 }
 
@@ -110,11 +148,134 @@ function buyTickets() {
         elmsArchive[i].style.display = 'none';
     }
     var elmsEvents = document.querySelectorAll("[id='events']");
-    for (var i = 0; i < elmsAbout.length; i++) {
+    for (var i = 0; i < elmsEvents.length; i++) {
         elmsEvents[i].style.display = 'none';
     }
     var elmsTickets = document.querySelectorAll("[id='buyTickets']");
-    for (var i = 0; i < elmsAbout.length; i++) {
+    for (var i = 0; i < elmsTickets.length; i++) {
         elmsTickets[i].style.display = 'Block';
     }
+    var elmsMerch = document.querySelectorAll("[id='merchPage']");
+    for (var i = 0; i < elmsMerch.length; i++) {
+        elmsMerch[i].style.display = 'none';
+    }
 }
+
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+
+function moveToDiv(scrollToDiv) {
+    console.log("here");
+    this.archive();
+    delayInMilliseconds = 170;
+    document.getElementById("startFrom").scrollIntoView();
+
+    setTimeout(function () {
+        document.getElementById(scrollToDiv).scrollIntoView({
+            behavior: `smooth`,
+        });
+    }, delayInMilliseconds);
+}
+
+function showWaitList() {
+    if (document.getElementById('waitList').style.display == 'none') {
+        document.getElementById('waitList').style.display = 'block';
+        $(document.getElementById('waitList')).animate({'height':'100%'},'slow')
+        document.getElementById('waitList').scrollIntoView({
+            behavior: `smooth`,
+        });
+        document.getElementById('navbar').style.display = 'none';
+    } else {
+        document.getElementById('waitList').style.display = 'none';
+    }
+}
+
+function showRegister(eventNo) {
+    console.log(eventNo);
+    if ($(document.getElementById(eventNo).parentElement).children('#register').length == 0) {
+        console.log($(document.getElementById(eventNo).parentElement).children('#register').length);
+        document.getElementById('navbar').style.display = 'none';
+        document.getElementById(eventNo).scrollIntoView({
+            behavior: `smooth`,
+        });
+        $(document.getElementById(eventNo).parentElement).animate({ 'margin-bottom': '800px' }, 'slow');
+        delayInMilliseconds = 400;
+        setTimeout(function () {
+            $(document.getElementById(eventNo).parentElement).append(this.createForm(eventNo));
+        }, delayInMilliseconds);
+        console.log();
+        $('input[type="checkbox"]').on('change', function() {
+            $('input[name="' + this.name + '"]').not(this).prop('checked', false);
+        });
+    } else {
+        console.log($(document.getElementById(eventNo).parentElement).children('#register').length);
+        $(document.getElementById(eventNo).parentElement).children('#register').remove();
+        delayInMilliseconds = 0;
+        setTimeout(function () {
+            document.getElementById(eventNo).scrollIntoView({
+                behavior: `smooth`,
+            });
+            $(document.getElementById(eventNo).parentElement).animate({ 'margin-bottom': '0px' }, 'slow');
+        }, delayInMilliseconds);
+    }
+
+}
+
+function createForm(eventNo) {
+    html = '<form action="/action_page.php" class="formRegister" id="register">';
+    html += '<img for="fname" src="img/Input Text Label.png" style="padding-top:5%"><br>'
+    html += '<input type="text" id="fname" name="fname" value="first and second name" class="textInput"><br>'
+    html += '<img for="fname" src="img/Input Text Label (1).png"><br>'
+    html += '<input type="text" id="lname" name="lname" value="email address" class="textInput"><br><br>'
+    html += '<img for="fname" src="img/Input Text Label (2).png"><br>'
+    html += '<input type="text" id="lname" name="lname" value="email address" class="textInput"><br><br>'
+    html += '<img for="fname" src="img/Input Text Label (3).png"><br>'
+    html += '<input type="text" id="lname" name="lname" value="email address" class="textInput"><br><br>'
+    html += '<img for="fname" src="img/Input Text Label (4).png"><br>'
+    html += '<input type="text" id="lname" name="lname" value="email address" class="textInput"><br><br>'
+    html += '<img for="fname" src="img/Input Text Label (5).png"><br>'
+    html += '<input type="text" id="lname" name="lname" value="email address" class="textInput"><br><br>'
+    html += '<div><input type="radio" id="male" name="male" value="male">'
+    html += '<label for="male" class="white">MALE</label><br><br>'
+    html += '<input type="radio" id="female" name="female" value="female">'
+    html += '<label for="female" class="white">FEMALE</label><br><br>'
+    html += '<input type="radio" id="other" name="other" value="others">'
+    html += '<label for="others" class="white">OTHERS</label><br><br></div>'
+    html += '<img src="img/Dropdown Title.png" for="OptionsForTickets"><br><select id="OptionsForTickets" name="OptionsForTickets"><option value="opt">PASS OPTIONS</option><option value="SOLO">SOLO PASS 750/-</option><option value="group">GROUP PASS /-(min, 3pax)</option><option value="vip">VIP PASS 1500/-(perks + merch)</option></select>'
+    html += '<div class="options_button"><img src="img/Buttons (1).png" onclick="showRegister('+eventNo+')"><img src="img/Buttons.png" class="completeButton"></div>'
+    html += '</form>'
+    console.log("reached here in create");
+    return html;
+}
+
+$(window).bind('mousewheel', function (e) {
+    document.getElementById('navbar').style.display = 'block';
+});
+
+
+$(document).ready(function () {
+    var NavTop = $('.navBarContainer').offset().top;
+    var Nav = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > NavTop) {
+            $('.navBarContainer').css('position', 'sticky');
+            // $('.navBarContainer').animate({'backdrop-filter':'blur(50px)'},'slow');
+            $('.navBarContainer').css('backdrop-filter', 'blur(20px)');
+        } else {
+            $('.navBarContainer').css('position', 'absolute');
+            $('.navBarContainer').css('backdrop-filter', 'blur(0px)');
+
+        }
+    };
+
+    Nav();
+
+    $(window).scroll(function () {
+        Nav();
+    });
+});
